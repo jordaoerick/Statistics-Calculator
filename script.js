61839fdd81aa234541072d58148e1f9fdfdb884d
jordaoerick <erickjordao@outlook.com>
2025-02-11 23:13:23 -0300

    updating some codes

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-const calculate = () => {}
+const getMean = (array) => array.reduce((acc, el) => acc + el, 0) / array.length;
+
+const getMedian = (array) => {
+  const sorted = array.toSorted((a, b) => a - b);
+  const median =
+    sorted.length % 2 === 0
+      ? getMean([sorted[sorted.length / 2], sorted[sorted.length / 2 - 1]])
+      : sorted[Math.floor(sorted.length / 2)];
+  return median;
+}
+
+const calculate = () => {
+  const value = document.querySelector("#numbers").value;
+  const array = value.split(/,\s*/g);
+  const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
+
+  const mean = getMean(numbers);
+
+
+  document.querySelector("#mean").textContent = mean;
+
+}
